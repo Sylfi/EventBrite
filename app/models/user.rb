b950809d06aa2dbfd46b1483921e35d7853ac101
Syle <sylvain.firer@gmail.com>
2019-08-12 14:49:27 +0200

    seed quasi-completed, migrations not made

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many: attendances
     has_many: events, through: :attendances
+    has_many: event_admin, foreign_key: 'administrator_id', class_name: "Event"
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+30.times do
+
+user = User.create!(email: Faker::Internet.email, description: Faker::Hacker.say_something_smart,
+first_name: Faker::FunnyName.name, last_name: Faker::Superhero.suffix)
+
+event = Event.create!(start_date: Faker::Date.forward(days: 120), duration: Faker::Number.between(from: 5, to: 1000),
+title: Faker::Books::CultureSeries.culture_ship, description: Faker::Quote.matz, price: Faker::Number.between(from: 1, to:100),
+location: Faker::Address.country_by_code(code: 'FR'), administrator: Faker::Number.between(from: 1, to: 10))
+
+
+
