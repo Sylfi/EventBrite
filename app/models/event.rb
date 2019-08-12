17a71c01df6b5bf4b6ab9b886ae4b26fcda5d312
Syle <sylvain.firer@gmail.com>
2019-08-12 13:29:49 +0200

    event model finished

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,15 @@
 class Event < ApplicationRecord
-    has_many: attendances
-    has_many: users, trough: :attendances
+    has_many :attendances
+    has_many :users, trough: :attendances
+    validates :start_date, presence: true
+    validates :duration, presence: true, confirmation: multiple_of_five
+    validates :title, presence: true, lenght: { in : 5..40 } 
+    validates :description, presence: true, lenght: {in : 20..1000}
+    validates :price, presence: true, greater_than: 0, less_than: 1001
+    validates :location, presence: true
+
+    def multiple_of_five?
+        (duration % 5) == 0
+    end
+
 end
